de67433ba0060919d98ca217e46dc066b1dbfa1f
Jahun <wkgjs0809@gmail.com>
2018-05-13 20:52:31 +0900

    Fix: func 'setTranslateQuery' moves to main.js

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
-const path = require('path');
+const path = require("path");
+const languageSet = require("./languageSet.json");
 
 module.exports = {
-    width: 300,
-    height: 400,
-    dir: path.join(__dirname, 'views'),
-    naverApi: {
-      detect: 'https://openapi.naver.com/v1/papago/detectLangs',
-      translate: 'https://openapi.naver.com/v1/papago/n2mt'
-    }
-}
+  width: 300,
+  height: 400,
+  dir: path.join(__dirname, "views"),
+  naverApi: {
+    detect: "https://openapi.naver.com/v1/papago/detectLangs",
+    translate: "https://openapi.naver.com/v1/papago/n2mt"
+  },
+  languageSet: languageSet
+};
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const ipcMain = require('electron').ipcMain;
-const menubar = require('menubar');
+const ipcMain = require("electron").ipcMain;
+const menubar = require("menubar");
 
-const apis = require('./lib/apis')
-const config = require('./config')
+const apis = require("./lib/apis");
+const config = require("./config");
 
 const mb = menubar({
   width: config.width,
@@ -10,27 +10,62 @@ const mb = menubar({
   dir: config.dir
 });
 
-mb.on('ready', function ready() {
+mb.on("ready", function ready() {
   // main process
-})
+});
 
-ipcMain.on('search', (e, text) => {
-  apis.detect(text)
+ipcMain.on("search", (e, text) => {
+  apis
+    .detect(text)
     .then(res => {
       // response example : { "langCode" : "ko" }
-      let query = apis.setTranslateQuery({ source: res.body.langCode, text });
+      let query = setTranslateQuery({ source: res.body.langCode, text });
       return apis.translate(query);
     })
     .then(res => {
-      const translatedText = res.body.message.result.translatedText
-      e.sender.send('async-search', true, translatedText)
+      const translatedText = res.body.message.result.translatedText;
+      e.sender.send("async-search", true, translatedText);
     })
     .catch(err => {
-      console.error(err)
-      e.sender.send('async-search', false, err)
-    })
-})
+      console.error(err);
+      e.sender.send("async-search", false, err);
+    });
+});
 
-ipcMain.on('hide', () => {
-  mb.app.hide()
-})
+ipcMain.on("hide", () => {
+  mb.app.hide();
+});
+
+function setTranslateQuery({ source, target, text } = {}) {
+  console.log("I'm in function, setTranslateQuery !");
+  // set target if 'target' is undefined.
+  if (source === "ko") target = target || "en";
+  else target = target || "ko";
+
+  // adjust source name of Chinese for query.
+  if (source === "zh-cn") source = "zh-CN";
+  else if (source === "zh-tw") source = "zh-TW";
+  // adjust target name of Chinese for query.
+  if (target === "zh-cn") target = "zh-CN";
+  else if (target === "zh-tw") target = "zh-TW";
+  console.log(source, target, text);
+
+  // there are 15 langCodes, including 'unknown'.
+  const langInfo = config.languageSet.langInfo;
+  const langCodes = config.languageSet.langCodes;
+  if (
+    !(
+      langInfo.translationKeys.includes(source) &&
+      langCodes[source].supportedTargets.includes(target)
+    )
+  ) {
+    // for a shift
+    console.log("not yet supported set.");
+    let say_sth = `${langCodes[source].name}에서 ${
+      langCodes[target].name
+    }으로의 변환은 아직 지원되지 않습니다.`;
+    [source, target, text] = ["ko", "en", say_sth];
+  }
+
+  return { source: source, target: target, text: text };
+}
