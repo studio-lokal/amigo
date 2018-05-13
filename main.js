45a794527c3d4476dd16cf72360bfbf7b501f9cb
Jahun <wkgjs0809@gmail.com>
2018-05-13 23:51:45 +0900

    Refactor: throw error when input is unsupported language.

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const menubar = require("menubar");
 const apis = require("./lib/apis");
 const config = require("./config");
 
+// there are 15 langCodes, including 'unknown'.
+const langInfo = config.languageSet.langInfo;
+const langCodes = config.languageSet.langCodes;
+
 const mb = menubar({
   width: config.width,
   height: config.height,
@@ -19,15 +23,22 @@ ipcMain.on("search", (e, text) => {
     .detect(text)
     .then(res => {
       // response example : { "langCode" : "ko" }
-      let query = setTranslateQuery({ source: res.body.langCode, text });
-      return apis.translate(query);
+      let source = res.body.langCode;
+      if (source === "unk") throw "어느 나라 말인가요??";
+      let query = setTranslateQuery({ source: source, text });
+      if (!isSupported(query)) {
+        throw `아직 ${langCodes[query.source].name}에서 ${
+          langCodes[query.target].name
+        }로 번역할 줄은 모르겠어요.`;
+      } else {
+        return apis.translate(query);
+      }
     })
     .then(res => {
       const translatedText = res.body.message.result.translatedText;
       e.sender.send("async-search", true, translatedText);
     })
     .catch(err => {
-      console.error(err);
       e.sender.send("async-search", false, err);
     });
 });
@@ -36,8 +47,20 @@ ipcMain.on("hide", () => {
   mb.app.hide();
 });
 
+function isSupported({ source, target, text } = {}) {
+  if (
+    !(
+      langInfo.translationKeys.includes(source) &&
+      langCodes[source].supportedTargets.includes(target)
+    )
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 function setTranslateQuery({ source, target, text } = {}) {
-  console.log("I'm in function, setTranslateQuery !");
   // set target if 'target' is undefined.
   if (source === "ko") target = target || "en";
   else target = target || "ko";
@@ -48,24 +71,6 @@ function setTranslateQuery({ source, target, text } = {}) {
   // adjust target name of Chinese for query.
   if (target === "zh-cn") target = "zh-CN";
   else if (target === "zh-tw") target = "zh-TW";
-  console.log(source, target, text);
 
-  // there are 15 langCodes, including 'unknown'.
-  const langInfo = config.languageSet.langInfo;
-  const langCodes = config.languageSet.langCodes;
-  if (
-    !(
-      langInfo.translationKeys.includes(source) &&
-      langCodes[source].supportedTargets.includes(target)
-    )
-  ) {
-    // for a shift
-    console.log("not yet supported set.");
-    let say_sth = `${langCodes[source].name}에서 ${
-      langCodes[target].name
-    }으로의 변환은 아직 지원되지 않습니다.`;
-    [source, target, text] = ["ko", "en", say_sth];
-  }
-
-  return { source: source, target: target, text: text };
+  return { source, target, text };
 }
